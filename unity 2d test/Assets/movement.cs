6711003e6f81cdfdd2a4dba1dcff47c886a03f84
alskiiie <39855812+alskiiie@users.noreply.github.com>
2018-07-28 16:36:57 +0200

    asdasd

--- unity 2d test/Assets/movement.cs
+++ unity 2d test/Assets/movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
 
+    public float speed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,31 @@ public class movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //moves object to the right
+        if (Input.GetKeyDown("d"))
+        {
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        }
+
+        //moves object to the left
+        if (Input.GetKeyDown("a"))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+        }
+
+        //moves object up
+        if (Input.GetKeyDown("w"))
+        {
+            transform.Translate(0f, speed * Time.deltaTime, 0f);
+        }
+
+        //moves object down
+        if (Input.GetKeyDown("s"))
+        {
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
+        }
+
+
+    }
 }
